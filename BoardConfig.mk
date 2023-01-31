dfb92f05859bca517616cffbf0fa744158712e0a
Andrey Sprynga <rtx4d@aospa.co>
2023-01-31 20:38:14 +0700

    beluga: Set correct brightness path
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,3 +61,6 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_ROUND_SCREEN := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 61
