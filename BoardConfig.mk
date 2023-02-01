40133e5836c60d4147a77c23bda4ced019026b66
Andrey Sprynga <rtx4d@aospa.co>
2023-02-01 14:22:22 +0700

    beluga: Use watch theme fo UI
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,12 +55,11 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_mdpi
+TW_THEME := watch_mdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_ROUND_SCREEN := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 61
