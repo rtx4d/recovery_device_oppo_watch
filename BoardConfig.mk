5fec776e8f8574dfb626d259f0dc3d67c2c374c8
Andrey Sprynga <rtx4d@aospa.co>
2023-02-02 16:04:33 +0700

    beluga: Set Cortex-A7 for CPU variant
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a7
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := beluga
